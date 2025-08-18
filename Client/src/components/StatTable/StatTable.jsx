1147092edca34888545b0f91545a124efc2e94a7
Simon Greenaway <Simon.gx01@gmail.com>
2025-08-18 17:28:05 -0400

    WR CSS
    Changes color for row when WR > 40 or > 50

--- Client/src/components/StatTable/StatTable.jsx
+++ Client/src/components/StatTable/StatTable.jsx
@@ -15,14 +15,24 @@ function StatTable({type, data}){
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              <td data-label="name">{type=="Player" ? row.player_name : row.commander_name}</td>
-              <td data-label="Games Played">{row.games}</td>
-              <td data-label="Wins">{row.wins}</td>
-              <td data-label="Win Rate (%)">{Math.ceil((row.wins / row.games) * 100)}%</td>
-            </tr>
-          ))}
+          {data.map((row, index) => {
+            const winRate = Math.round((row.wins / row.games) * 100);
+            let className = '';
+            if (winRate >= 50) {
+              className = 'wr-too-high';
+            } else if (winRate >= 40) {
+              className = 'wr-high';
+            }
+            
+            return (
+              <tr key={index} className={className}>
+                <td data-label="name">{type=="Player" ? row.player_name : row.commander_name}</td>
+                <td data-label="Games Played">{row.games}</td>
+                <td data-label="Wins">{row.wins}</td>
+                <td data-label="Win Rate (%)">{winRate}%</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
